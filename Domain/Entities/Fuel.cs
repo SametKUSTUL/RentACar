9052a6d9b93a367e55d8edfb360bdf2ed3550527
samet.kustul <samet.kustul@unitedpayment.com>
2023-11-01 00:19:03 +0300

    Add project files.

---
+++ Domain/Entities/Fuel.cs
@@ -0,0 +1,21 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Fuel : Entity<Guid>
+{
+    public string? Name { get; set; }
+    public virtual ICollection<Model> Models { get; set; }
+
+    public Fuel()
+    {
+        Models = new List<Model>();
+    }
+
+    public Fuel(Guid id, string name) : this()
+    {
+        Id = id;
+        Name = name;
+    }
+
+}
